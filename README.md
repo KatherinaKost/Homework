d9a4ba621623426fc1acae858900fde5fd8c531f
Katherina <katya_71296@mail.ru>
2025-07-20 22:31:13 +0300

    1

--- README.md
+++
@@ -1,2 +0,0 @@
-# Lesson2
-«# Welcome Git»
--- lesson 3/6.py
+++
@@ -1,8 +0,0 @@
-'''
-Пользователь вводит 3 числа, 
-найти среднее арифметическое с точность 3 знака после запятой
-
-'''
-var_1, var_2, var_3 = int(input()), int(input()), int(input())
-average = (var_1 + var_2 + var_3)/3
-print(round(average, 3))
